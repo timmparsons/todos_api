d7f83f8b7741efc00f8e4257486377222ebc5b51
Tim Parsons <tim@Tims-MacBook-Pro.local>
2018-08-23 23:04:53 -0700

    added addTodo function

---
+++ public/app.js
@@ -0,0 +1,12 @@
+/* global */
+$(document).ready(function() {
+  $.getJSON("/api/todos")
+  .then(addTodos)
+});
+
+function addTodos(todos) {
+  todos.forEach(function(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    $('.list').append(newTodo);
+  })
+};
